0242768cfa4f5782f45325b8837ad8d86d6fbb9b
Luber <luberon90@gmail.com>
2019-11-12 17:32:44 +0100

    walniecie structa, macierze dzialaja, dystans nie.

--- TSP/TSP.cpp
+++ TSP/TSP.cpp
@@ -8,136 +8,145 @@
 //#include "C:\Users\piotr\source\repos\TSP algorithm\TSP algorithm\ACO optimalization.cpp"
 
 using namespace std;
-int* path;
-int** verticals;
-double** matrix, dist;
-bool* visited;
 
+struct Dane {
+	int** verticals, * path, vnumber;
+	double** matrix, dist = 0;
+	bool* visited;
+};
 
-void distance(int vnumber)
+void distance(Dane dane)
 {
 	int i, j;
 	double x1, x2, y1, y2;
-	for (i = 0; i < vnumber; i++)
+	for (i = 0; i < dane.vnumber; i++)
 	{
-		for (j = i; j < vnumber; j++)
+		for (j = i; j < dane.vnumber; j++)
 		{
 			if (i != j)
 			{
-				x1 = verticals[i][0];
-				y1 = verticals[i][1];
-				x2 = verticals[j][0];
-				y2 = verticals[j][1];
-				matrix[i][j] = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
-				matrix[j][i] = matrix[i][j];
+				x1 = dane.verticals[i][0];
+				y1 = dane.verticals[i][1];
+				x2 = dane.verticals[j][0];
+				y2 = dane.verticals[j][1];
+				dane.matrix[i][j] = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+				dane.matrix[j][i] = dane.matrix[i][j];
 			}
 			else
 			{
-				matrix[i][j] = 0;
+				dane.matrix[i][j] = 0;
 			}
 		}
 	}
 }
-void find_path(int v, int vnumber, int counter)
+void find_path(int v, Dane dane, int counter)
 {
 	int i, j;
 	double min = 9999;
-	visited[v] = 1;
-	path[counter++] = v;
-	for (j = 0; j < vnumber; j++)
+	dane.visited[v] = 1;
+	dane.path[counter++] = v;
+	for (j = 0; j < dane.vnumber; j++)
 	{
-		if (matrix[v][j] < min && visited[j] == 0)
+		if (dane.matrix[v][j] < min && dane.visited[j] == 0)
 		{
-			min = matrix[v][j];
+			min = dane.matrix[v][j];
 			i = j;
 		}
 	}
-	if (counter < vnumber) {
-		dist += min;
-		find_path(i, vnumber, counter);
+	if (counter < dane.vnumber) {
+		dane.dist += min;
+		find_path(i, dane, counter);
 	}
 	else
 		return;
 }
-void TSP(int vnumber, int v)
+void TSP(int v, Dane dane)
 {
 	int j;
 	//v = rand() % vnumber;
 	v = v - 1;
-	find_path(v, vnumber, 0);
-	for (j = 0; j < vnumber; j++)
+	find_path(v, dane, 0);
+	for (j = 0; j < dane.vnumber; j++)
 	{
-		visited[j] = 0;
-		cout << path[j] + 1 << " ";
+		dane.visited[j] = 0;
+		cout << dane.path[j] + 1 << " ";
 	}
-	dist += matrix[v][path[j - 1]];
-	cout << path[0] + 1 << endl;
+	dane.dist += dane.matrix[v][dane.path[j - 1]];
+	cout << dane.path[0] + 1 << endl;
 }
-void show(int vnumber)
+void show(Dane dane)
 {
 	int i, j;
-	for (i = 0; i < vnumber; i++)
+	for (i = 0; i < dane.vnumber; i++)
 	{
-		for (j = 0; j < vnumber; j++)
+		for (j = 0; j < dane.vnumber; j++)
 		{
-			cout << matrix[i][j] << " ";
+			cout << dane.matrix[i][j] << " ";
 		}
 		cout << endl;
 	}
 }
-int main()
-{
-	srand(time(NULL));
-	int vnumber, i, j, null;
 
-	//Generujemy plik
-	//gen(11, "plik.txt");
 
-	//Otwieramy plik, rozwijamy tablice oraz zapisujemy wierzchołki z pliku
+
+Dane wczytaj(string nazwa) {
 	ifstream plik;
+	Dane dane;
+	int num;
 	plik.open("plik.txt", ios::in);
 	if (plik.good() == true)
 	{
 		//zapisujemy liczbe wierzcholkow
-		plik >> vnumber;
+		plik >> dane.vnumber;
 
 		//tablica z odleglosciami
-		matrix = new double* [vnumber];
+		dane.matrix = new double* [dane.vnumber];
 
 
 
 		//tablica z wspolrzednymi wierzcholkow
-		verticals = new int* [vnumber];
+		dane.verticals = new int* [dane.vnumber];
 
 		//tablica odwiedzonych
-		visited = new bool[vnumber];
+		dane.visited = new bool[dane.vnumber];
 
 		//tablica sciezki
-		path = new int[vnumber];
+		dane.path = new int[dane.vnumber];
 
-		for (i = 0; i < vnumber; i++)
+		for (int i = 0; i < dane.vnumber; i++)
 		{
-			matrix[i] = new double[vnumber];
+			dane.matrix[i] = new double[dane.vnumber];
 
 
 
-			verticals[i] = new int[2];
+			dane.verticals[i] = new int[2];
 
-			visited[i] = 0;
+			dane.visited[i] = 0;
 		}
 		while (!plik.eof())
 		{
-			plik >> null;
-			plik >> verticals[null - 1][0];
-			plik >> verticals[null - 1][1];
+			plik >> num;
+			plik >> dane.verticals[num - 1][0];
+			plik >> dane.verticals[num - 1][1];
 		}
 		plik.close();
+		return dane;
 	}
-	distance(vnumber);
+}
+
+int main()
+{
+	srand(time(NULL));
+	Dane dane;
+	//Otwieramy plik, rozwijamy tablice oraz zapisujemy wierzchołki z pliku
+	dane = wczytaj("plik.txt");
+	cout << dane.verticals[0][1]<<endl;
+
+	distance(dane);
 	//show(vnumber);
-	TSP(vnumber, 1);
+	TSP(1, dane);
 	//TSP_ACO(vnumber);
-	cout << "Dystans: " << dist << endl;
+	cout << "Dystans: " << dane.dist << endl;
 	system("PAUSE");
 	return(0);
 }
